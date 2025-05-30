059ffeae5fc44f1310aff454ad4c380b4d32659e
manesh41 <maneshpoonia7297@gmail.com>
2025-05-30 09:00:09 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,4 +1,4 @@
-api_id = "24473318"
-api_hash = "e7dd0576c5ac0ff8f90971d6bb04c8f5"
-bot_token = "8034069514:AAHUBpzSCq41jPwsJkDbXuEoVC_yCxzyuw0"
-auth_users = [5840594311]
+api_id = "24858402"
+api_hash = "eeff2fe9ee10d03c5c02177c60bcd580"
+bot_token = "8067995232:AAEtqu-QOaJ2L6dNCJwmFH9PPmp_39yxUy0"
+auth_users = [7092107565]
